4c8473aa1fd5e867259f43c9b662c8fc6c71ff03
dgakis <dgakis@arx.net>
2025-02-10 14:49:48 +0200

    add home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
 
 export const routes: Routes = [
     { path: '', component: NavbarComponent },
+    { path: '', component: HomeComponent },
 ];
