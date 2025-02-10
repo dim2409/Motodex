271ab6ee40258ca2d18d6997aea698a488d4e071
dgakis <dgakis@arx.net>
2025-02-10 14:22:24 +0200

    Initial commit - Setup Angular project with LESS and routing

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import { provideRouter } from '@angular/router';
+import { routes } from './app/app.routes';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  ...appConfig,
+  providers: [provideRouter(routes)]
+}).catch((err) => console.error(err));
